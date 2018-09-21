1f0d319dca385719b6e0d4dd17153f65b2ec64d2
srithep444 <srithep555@gmail.com>
2018-09-21 22:49:30 +0700

    Game Logic เพิ่มวิดีโอให้รับชม

--- src/WordCard.js
+++ src/WordCard.js
@@ -49,9 +49,8 @@ export default class WordCard extends Component {
           />
         ))}
         <h2>{this.state.complete? "You Win!!!" : "Select word here."}</h2> 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FlI4D59nH7I?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
-      
-      
     );
   }
 }
