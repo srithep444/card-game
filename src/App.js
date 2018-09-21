53fa538698a3e3341ce3e83ea9852d8e4963387c
srithep444 <srithep555@gmail.com>
2018-09-21 16:11:18 +0700

    CharacterCard วนซ้ำเพื่อสร้างคอมโพเนนต์หลายๆ คอมโพเนนต์พร้อมกัน

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import './App.css';
 
+const word = "Hello";
+
 class App extends Component {
+    
     render() {
     return (
       <div>
-        <CharacterCard value="h"/> 
-        <CharacterCard value="i"/>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
       </div>
     );
   }
